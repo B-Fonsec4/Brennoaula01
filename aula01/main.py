26be8f21d4dec227e688cef664b961294d3455f6
B-Fonsec4 <mottabrenno@gmail.com>
2024-09-19 19:10:44 -0300

    diretório da aula 01

---
+++ aula01/main.py
@@ -0,0 +1,54 @@
+# print('Olá Mundo!!!')
+# print('Brenno')
+ 
+# idade = 22
+# nome = 'Pedro'
+# sobrenome = 'Silva'
+# print(nome + sobrenome)
+
+# ano_nascimento = 1999
+# ano_atual = 2024
+# idade = ano_atual - ano_nascimento
+# print(idade)
+
+# n1 = 5  
+# n2 = 2
+
+# resultado = n1 // n2
+# print(resultado)
+
+# Letra A
+# print('Hello world')
+
+# Letra B
+# mensagem = 'Algoritimo em Python'
+# print(mensagem)
+
+# Letra C
+# nome = 'Brenno'
+# print("Ola", nome)
+
+# Letra D
+# nome = 'Brenno'
+# cor_favorita = 'Vermelho'
+# print(f'Olá {nome}, sua cor favorita é {cor_favorita}')
+
+# Letra E
+# nome = 'Brenno' 
+# profissao = 'Uber'
+# onde_mora = 'São Gonçalo'
+# print(f'Ola {nome} sua profissao é {profissao} e vc mora em {onde_mora}.')
+
+# Letra F
+
+nome = input('Escreva seu nome: ')
+ano_nascimento = int(input('Escreva seu ano de nascimento: '))
+altura = float(input('Escreva sua altura: '))
+idade = 2024 - ano_nascimento
+print(f'Ola {nome} você nasceu em {ano_nascimento}  e mede {altura}cm.')
+print(f' A sua idade é  {idade}')
+# print(type(ano_nascimento)) mostra o tipo da variavel
+
+
+
+
